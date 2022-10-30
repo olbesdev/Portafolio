059e0bfdcdf1eec26e5500c7e6bd340d07fa7b9a
Jonathan Olbes <olbesdev@gmail.com>
2022-10-29 21:19:21 -0500

    Creando carpeta componentes y configurando Tailwinds

---
+++ mywebsite/src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="Container w-full">
+            <h3 className="sm: text-left md:text-center lg:text-right">My App</h3>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ mywebsite/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return ( 
+        <div className="max-w-full">
+            <div>
+                <h1 className="sm: text-left md:text-center lg:text-right">Hola NavBar</h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default NavBar;
